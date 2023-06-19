21ca4e34dd2dbedf30e164c8ac9430eed7720948
thanhbinh truong <thanhbinhtruong678@gmail.com>
2023-06-19 22:19:09 +0700

    commit

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# Trick to get directory that script is located in
+PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker-compose -f ${PATH}/docker-compose-build.yaml push
